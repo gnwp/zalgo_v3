6625d0312d4772e70677dc296e23f50ee675cd69
gnwp <gnwp>
2014-11-26 14:26:15 +0100

    Add configurable namespace for memcached client

--- settings.rb
+++ settings.rb
@@ -1,5 +1,5 @@
 
 DB = Sequel.connect("postgres://antifa:antifa@127.0.0.1/antifa")
-CACHE_CLIENT = Dalli::Client.new( '127.0.0.1:11211', :value_max_bytes => 5242880 )
+CACHE_CLIENT = Dalli::Client.new( '127.0.0.1:11211', :value_max_bytes => 5242880, :namespace => "antifa" )
 SPHINX = Sequel.connect("mysql2://127.0.0.1/sphinx?port=9306")
 SPHINX_T = "doc1"
